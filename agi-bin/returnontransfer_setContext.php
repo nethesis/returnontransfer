3b99fd1002f81349931c9250f8bdcbbc8406ac06
Andrew Nagy <andrew.nagy@the159.com>
2018-10-29 01:33:02 -0400

    Cleanup and conform to FreePBX standards -No need for a separate key value store when a KVStore exists natively in FreePBX -No Need to set FreePBX settings directly in the database, this should alway happen through the Config class -Move dialplan functions into BMO object -Remove install.php and uninstall.php -Move install code into BMO object -Fix agi file to not load ALL of FreePBX

--- agi-bin/returnontransfer_setContext.php
+++ agi-bin/returnontransfer_setContext.php
@@ -9,7 +9,7 @@
 #
 #    ReturnOnTransfer module is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or any 
+#    the Free Software Foundation, either version 3 of the License, or any
 #    later version.
 #
 #    ReturnOnTransfer module is distributed in the hope that it will be useful,
@@ -20,40 +20,41 @@
 #    You should have received a copy of the GNU General Public License
 #    along with ReturnOnTransfer module.  If not, see <http://www.gnu.org/licenses/>.
 #
+$restrict_mods = true; //dont load any functions.inc.phps
+include "/etc/freepbx.conf";
+require_once "phpagi.php";
 
-include_once ("/etc/freepbx.conf");
-define("AGIBIN_DIR", "/var/lib/asterisk/agi-bin");
-include(AGIBIN_DIR."/phpagi.php");
-
-global $db;
-
+$db = FreePBX::create()->Database;
 $agi = new AGI();
 $extension = $argv[1];
 $callernum = $argv[2];
 $blindnum = $argv[3];
 
 //get xfer dest context
-$sql2 = "SELECT data FROM sip where keyword=\"context\" and id=\"$extension\";";
-$result = @$db->getRow($sql2);
-$extension_context = $result[0];
+$sql2 = "SELECT data FROM sip where `keyword`='context' and `id` = :extension";
+$sth = $db->prepare($sql2);
+$sth->execute(array(':extension' => $extension));
+$extension_context = $sth->fetch(\PDO::FETCH_COLUMN);
 
 //check context of xfer dest
 if($extension_context!='') {
-         @$agi->exec("Set", "xfer_context=$extension_context");
+  $agi->exec("Set", "xfer_context=$extension_context");
 } else {
-         //get caller context
-         $sql3 = "SELECT data FROM sip where keyword=\"context\" and id=\"$callernum\";";
-         $result = @$db->getRow($sql3);
-         $callerid_context = $result[0];
+  //get caller context
+  $sql3 = "SELECT data FROM sip where `keyword`='context' and `id` = :callernum";
+  $sth = $db->prepare($sql3);
+  $sth->execute(array(':callernum' => $callernum));
+  $callerid_context = $sth->fetch(\PDO::FETCH_COLUMN);
 
-         //check context of caller
-         if ($callerid_context!='') {
-                @$agi->exec("Set", "xfer_context=$callerid_context");
-         } else {
-                //get blind context
-                $sql4 = "SELECT data FROM sip where keyword=\"context\" and id=\"$blindnum\";";
-                $result = @$db->getRow($sql4);
-                $blind_context = $result[0];
-                @$agi->exec("Set", "xfer_context=$blind_context");
-         }
+  //check context of caller
+  if ($callerid_context!='') {
+    $agi->exec("Set", "xfer_context=$callerid_context");
+  } else {
+    //get blind context
+    $sql4 = "SELECT data FROM sip where `keyword`='context' and `id` = :blindnum";
+    $sth = $db->prepare($sql4);
+    $sth->execute(array(':blindnum' => $callernum));
+    $blind_context = $sth->fetch(\PDO::FETCH_COLUMN);
+    @$agi->exec("Set", "xfer_context=$blind_context");
+  }
 }
